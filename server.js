589d9cfb4cbb6799b430d035b21498de7f798170
user <pengfluf@tutanota.com>
2018-04-13 14:20:23 +0300

    Express App Initialized

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const http = require('http');
+const app = require('./app');
 
 const PORT = process.env.PORT || 3002;
 
-const server = http.createServer();
+const server = http.createServer(app);
 
 server.listen(PORT);
 
