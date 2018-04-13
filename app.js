2688e1f032f2e0d4e22d5b056dfbfb1d7bd8d371
user <pengfluf@tutanota.com>
2018-04-13 16:50:20 +0300

    CORS Handling

--- app.js
+++ app.js
@@ -16,6 +16,26 @@ app.use(bodyParser.urlencoded({
   extended: false,
 }));
 
+// Headers for CORS Handling
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res
+      .header(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, PUT, DELETE'
+      );
+    return res
+      .status(200)
+      .json({});
+  }
+  next();
+});
+
 // Routes
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
