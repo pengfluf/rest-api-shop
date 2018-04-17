412053cf782be5d72dc0c105f385fd582eecb5fe
user <pengfluf@tutanota.com>
2018-04-17 05:04:04 +0300

    Products Requests Basic Full Working Setting

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,38 +5,9 @@ const mongoose = require('mongoose');
 const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
-  res
-    .status(200)
-    .json({
-      message: 'It came from GET /products',
-    });
-});
-
-router.post('/', (req, res, next) => {
-  const { name, price } = req.body;
-  const product = new Product({
-    _id: new mongoose.Types.ObjectId(),
-    name,
-    price,
-  });
-  product
-    .save()
-    .then(
-      (result) => console.log(result),
-      (err) => console.error(err)
-    );
-  res
-    .status(201)
-    .json({
-      message: 'It came from POST /products',
-      product,
-    });
-});
-
-router.get('/:productID', (req, res, next) => {
-  const { productID } = req.params;
-
-  Product.findById(productID)
+  Product
+    .find()
+    // exec is needed for Promise
     .exec()
     .then(
       (result) => {
@@ -49,6 +20,71 @@ router.get('/:productID', (req, res, next) => {
         console.error(err);
         res
           .status(500)
+          .json({
+            error: err,
+          });
+      }
+    );
+});
+
+router.post('/', (req, res, next) => {
+  const { name, price } = req.body;
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name,
+    price,
+  });
+  product
+    .save()
+    .then(
+      (result) => {
+        console.log(result);
+        res
+          .status(201)
+          .json({
+            message: 'It came from POST /products',
+            product,
+          });
+      },
+      (err) => {
+        console.error(err);
+        res
+          .status(500)
+          .json({
+            error: err,
+          });
+      }
+    );
+});
+
+router.get('/:productID', (req, res, next) => {
+  const { productID } = req.params;
+
+  Product.findById(productID)
+    // exec is needed for Promise
+    .exec()
+    .then(
+      (result) => {
+        console.log(result);
+        if (result) {
+          res
+            .status(200)
+            .json(result);
+        } else {
+          console.log('There\'s no such product.');
+          res
+            .status(404)
+            .json({
+              error: {
+                message: 'There\'s no such product.',
+              },
+            });
+        }
+      },
+      (err) => {
+        console.error(err);
+        res
+          .status(500)
           .json({ error: err });
       }
     );
@@ -56,40 +92,66 @@ router.get('/:productID', (req, res, next) => {
 
 router.patch('/:productID', (req, res, next) => {
   const { productID } = req.params;
-  // Using testing ID
-  if (productID === 'correct') {
-    res
-      .status(200)
-      .json({
-        message: 'It came from PATCH /products/:productID and ID is correct',
-        productID,
-      });
-  } else {
-    res
-      .status(200)
-      .json({
-        message: 'It came from PATCH /products/:productID and ID IS NOT correct',
-      });
-  }
+  const updateOps = {};
+
+  Object.entries(req.body)
+    .forEach((operation) => {
+      // operation[0] is the key
+      // operation[1] is the value
+      // just don't want to declare
+      // a new variable for each iteration
+      updateOps[operation[0]] = operation[1];
+    });
+  console.log(updateOps);
+
+  Product
+    .update(
+      { _id: productID },
+      { $set: updateOps }
+    )
+    .exec()
+    .then(
+      (result) => {
+        console.log(result);
+        res
+          .status(200)
+          .json(result);
+      },
+      (err) => {
+        console.error(err);
+        res
+          .status(500)
+          .json({
+            error: err,
+          });
+      }
+    );
 });
 
 router.delete('/:productID', (req, res, next) => {
   const { productID } = req.params;
-  // Using testing ID
-  if (productID === 'correct') {
-    res
-      .status(200)
-      .json({
-        message: 'It came from DELETE /products/:productID',
-        productID,
-      });
-  } else {
-    res
-      .status(200)
-      .json({
-        message: 'It came from DELETE /products/:productID and ID IS NOT correct',
-      });
-  }
+
+  Product.remove({
+    _id: productID,
+  })
+    // exec is needed for Promise
+    .exec()
+    .then(
+      (result) => {
+        console.log(result);
+        res
+          .status(200)
+          .json(result);
+      },
+      (err) => {
+        console.error(err);
+        res
+          .status(500)
+          .json({
+            error: err,
+          });
+      }
+    );
 });
 
 module.exports = router;
